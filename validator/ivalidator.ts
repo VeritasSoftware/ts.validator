7cf6c7889aad7420ab4edaea888380a57ca212be
VeritasSoftware <shantanu_negi@hotmail.com>
2018-04-08 22:13:39 +1000

    terminate sentence typo fix.

--- validator/ivalidator.ts
+++ validator/ivalidator.ts
@@ -5,7 +5,7 @@ export interface IValidator<T> {
     NotNull<TProperty>(predicate: Func<T, TProperty>, message: string, errorIdentifier?: string): IValidator<T>;
     IsNull<TProperty>(predicate: Func<T, TProperty>, message: string, errorIdentifier?: string): IValidator<T>;
     NotEmpty(predicate: Func<T, string>, message: string, errorIdentifier?: string): IValidator<T>;
-    IsEmpty(predicate: Func<T, string>, message: string, errorIdentifier?: string): IValidator<T> 
+    IsEmpty(predicate: Func<T, string>, message: string, errorIdentifier?: string): IValidator<T>;
     Matches(predicate: Func<T, string>, regex: string, message: string, errorIdentifier?: string): IValidator<T>;
     NotMatches(predicate: Func<T, string>, regex: string, message: string, errorIdentifier?: string): IValidator<T>;
     CreditCard(predicate: Func<T, number>, message: string, errorIdentifier?: string): IValidator<T>;
