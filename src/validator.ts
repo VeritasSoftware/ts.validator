ea100e389ed8dc8f91798513b4e8e4cb7b7e2a1a
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-28 00:32:49 +1000

    added Base/BaseAsync api for managing inheritance in framework.

--- src/validator.ts
+++ src/validator.ts
@@ -12,7 +12,7 @@ export class ObjectValidator<T> implements IValidator<T> {
         this._model = model;
         this._validationErrors = new Array<ValidationError>();
 
-        this._clonedModel = TypeFactory.getTypeClone(model);  
+        this._clonedModel = TypeFactory.getTypeClone(model);      
     }
 
     For<TProperty>(predicate: Func<T, TProperty>, ruleSet: Func<IRuleSetValidator<T, TProperty>, IValidationResult>): IValidator<T> {
@@ -291,12 +291,46 @@ class RuleSetValidator<T, TProperty> implements IRuleSetValidator<T, TProperty> 
     }  
 }
 
+class BaseRule<T> {
+    _model: T;
+    _rules: Func<IValidator<T>, ValidationResult>;
+
+    constructor(model: T, rules: Func<IValidator<T>, ValidationResult>) {
+        this._model = model;
+        this._rules = rules;
+    }
+}
+
 export class Validator<T> implements IValidatorSync<T>, IValidatorAsync<T> {
-    _model: T;    
+    _model: T;
+    _validationErrors: Array<ValidationError>;
+    _basesAsync: Array<BaseRule<any>>;    
 
     constructor(model: T)
     {
-        this._model = model;        
+        this._model = model;   
+        this._validationErrors = new Array<ValidationError>();
+        this._basesAsync = new Array<BaseRule<any>>();             
+    }
+
+    Base<TBase extends T>(rules: Func<IValidator<TBase>, ValidationResult>): IValidatorSync<T> {
+        var base = <TBase>this._model; 
+
+        if (base != null) {
+            var validationResult = new Validator<TBase>(base).Validate(rules);
+            if (validationResult.Errors.length > 0) {
+                validationResult.Errors.forEach(error => this._validationErrors.push(error));
+            }
+        }
+
+        return this;        
+    }
+
+    BaseAsync<TBase extends T>(rules: Func<IValidator<TBase>, ValidationResult>): IValidatorAsync<T> {
+        var base = <TBase>this._model;
+        this._basesAsync.push(new BaseRule<TBase>(base, rules));        
+
+        return this;        
     }
 
     Validate(rules: Func<IValidator<T>, ValidationResult>): ValidationResult {
@@ -305,7 +339,23 @@ export class Validator<T> implements IValidatorSync<T>, IValidatorAsync<T> {
 
     async ValidateAsync(rules: Func<IValidator<T>, ValidationResult>): Promise<ValidationResult> {
         var promise = new Promise<ValidationResult>((resolve, reject) => {
-            resolve(rules(new ObjectValidator<T>(this._model)));
+
+            if (this._basesAsync.length > 0) {
+                this._basesAsync.forEach(async base => {
+                    var validationResult = base._rules(new ObjectValidator<any>(base._model));
+                    if (validationResult.Errors.length > 0) {
+                        validationResult.Errors.forEach(error => this._validationErrors.push(error));
+                    }
+                });                
+            }
+
+            var validationResult = rules(new ObjectValidator<T>(this._model));
+
+            if (validationResult.Errors.length > 0) {
+                validationResult.Errors.forEach(error => this._validationErrors.push(error));
+            }
+
+            resolve(new ValidationResult(this._validationErrors));
         });
         
         return promise;
